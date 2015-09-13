aa25f1b52373c8511ae678d8fcf3d5750572d1b1
ssswagatss <ssswagatss@gmail.com>
2015-09-13 22:01:33 +0530

    Completed

---
+++ PartialView/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using PartialView.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartialView.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult TopMenu() // This action is used for menu 
+        {
+            IEnumerable<Menu> menutbl = Menu.GetMenus();
+
+
+            return PartialView(menutbl);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ PartialView/Models/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartialView.Models
+{
+    public class Menu
+    {
+        public string MenuId { get; set; }
+        public string MenuDescription { get; set; }
+
+        public static List<Menu> GetMenus()
+        {
+            List<Menu> menus = new List<Menu>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                menus.Add(new Menu() { MenuId=i.ToString(),
+                                        MenuDescription="Menu - " + i.ToString()});
+            }
+            return menus;
+        }
+    }
+    
+
+}
